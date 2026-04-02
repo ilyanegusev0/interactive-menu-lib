02c8592f922ef45ea000704ea57d8afec26539b2
ilyanegusev <ilyanegusev0912@gmail.com>
2026-04-02 18:29:34 +0300

    Make OptionItem value mandatory

--- src/Items/OptionItem.cs
+++ src/Items/OptionItem.cs
@@ -12,7 +12,7 @@ namespace InteractiveMenu.Items
         /// <summary>
         /// Gets the value associated with this option.
         /// </summary>
-        public object? Value { get; }
+        public object Value { get; }
 
         // CONSTRUCTORS
 
@@ -22,40 +22,17 @@ namespace InteractiveMenu.Items
         /// <param name="text">The display text.</param>
         /// <param name="value">The value returned when selected.</param>
         /// <param name="color">The foreground color.</param>
-        public OptionItem(string text, object? value, ConsoleColor? color = null)
+        public OptionItem(string text, object value, ConsoleColor? color = null)
             : base(text, color)
         {
             Value = value;
         }
 
-        /// <summary>
-        /// Initializes a new instance of <see cref="OptionItem"/> with text only.
-        /// </summary>
-        /// <param name="text">The display text.</param>
-        public OptionItem(string text)
-            : this(text, null, null) { }
-
-        /// <summary>
-        /// Initializes a new instance of <see cref="OptionItem"/> with text and value.
-        /// </summary>
-        /// <param name="text">The display text.</param>
-        /// <param name="value">The value returned when selected.</param>
-        public OptionItem(string text, object value)
-            : this(text, value, null) { }
-
-        /// <summary>
-        /// Initializes a new instance of <see cref="OptionItem"/> with text and color.
-        /// </summary>
-        /// <param name="text">The display text.</param>
-        /// <param name="color">The foreground color.</param>
-        public OptionItem(string text, ConsoleColor color)
-            : this(text, null, color) { }
-
         // METHODS
 
         // internal
 
-        object? ISelectable.OnSelect()
+        object ISelectable.OnSelect()
         {
             return Value;
         }
